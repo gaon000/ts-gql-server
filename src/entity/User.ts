81a181b499f6ad348f632789eeded092b691ef4c
gaon000 <gaon000@naver.com>
2021-09-29 03:33:06 +0900

    setup typeorm

---
+++ src/entity/User.ts
@@ -0,0 +1,24 @@
+import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+
+@Entity("bob")
+export class User {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({type: "text", unique: true })
+    email: string;
+
+    @Column({type: "bool", default: false})
+    confirmed: boolean;
+
+    @Column({type: "varchar", length: "230"})
+    firstName: string;
+
+    @Column("text")
+    lastName: string;
+
+    @Column()
+    age: number;
+
+}
